a75cb69b6a75b1e8e140f54ccd2d42499f7aa8c0
Victor Yeo <you@example.com>
2020-06-06 17:35:10 +0800

    use rinkeby as web3 provider

--- src/web3/getWeb3.js
+++ src/web3/getWeb3.js
@@ -1,13 +1,26 @@
 import Web3 from 'web3'
 import { store } from '../store/store'
-import { ganachehost } from './constants'
-import { web3Initialized } from '../action/actions.js'
+import { ganachehost, rinkeby } from './constants'
+import { web3Initialized } from '../actions/actions.js'
+
+/*
+export const WEB3_INITIALIZED = 'WEB3_INITIALIZED'
+
+export const web3Initialized = (results) => {
+  return {
+    type: actionTypes.WEB3_INITIALIZED,
+    payload: results,
+  }
+};*/
 
 const getWeb3 = new Promise((resolve, reject) => {
   let results
   let web3 = {}
+
+  mnemonic = "pride auto solar tomorrow trim dismiss myth alert scrap gap clean rotate"
+
   // Wait for loading completion to avoid race conditions with web3 injection timing.
-  web3 = new Web3(new Web3.providers.HttpProvider(ganachehost))
+  web3 = new Web3(new Web3.providers.HttpProvider(rinkeby))
   results = {
     web3Instance: web3,
   }
