76d0bdfafc9729bfec675cff29df3122bd8a52d8
Victor Yeo <you@example.com>
2020-06-06 17:34:23 +0800

    add rinkeby testnet

--- src/web3/constants.js
+++ src/web3/constants.js
@@ -5,6 +5,8 @@ export const localStorageKey = 'ks'
 
 export const ganachehost = 'http://localhost:7545'
 
+export const rinkeby = "https://rinkeby.infura.io/v3/2cdeb2ee72bb4c6caebd580bacc16769"
+
 /*usage: convert amount to wei
 const sendAmount = new BigNumber(amount).times(Ether);
 */
