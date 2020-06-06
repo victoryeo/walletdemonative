d4a8a627f89d70fa182a01efa21e1aeb60bb6ea6
Victor Yeo <you@example.com>
2020-06-06 17:32:31 +0800

    add check network , check account, add dispatch

--- src/components/Web3Info.js
+++ src/components/Web3Info.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
 import {connect} from "react-redux"
 import { STPupdateAccounts } from '../actions/actions.js'
+import * as Utils from '../web3/utils'
 
 class Web3Info extends Component {
   constructor(props) {
@@ -15,9 +16,26 @@ class Web3Info extends Component {
 
     const web3Returned = setInterval(() => {
       if (this.props.web3 != null) {
-        console.log(this.props.web3)
         clearInterval(web3Returned);
-        this.web3 = this.props.web3.web3Instance;
+        this.web3 = this.props.web3.web3Instance
+            //console.log(this.web3)
+        Utils.checkNetwork(this.web3).then((res) => {
+            console.log(res)
+            if (res == 'local' || res == 'rinkeby') {
+              this.setState({
+                isConnected: true,
+             })
+            }
+        })
+        try {
+          console.log('vic')          
+          Utils.checkBalance(this.web3, this.props.STPupdateAccounts);
+
+            //console.log(this.props.account)
+        } catch (err) {
+          console.error('error', err);
+        }
+
       }
     },1000)
   }
@@ -26,7 +44,8 @@ class Web3Info extends Component {
     return (
       <View style={styles.container}>
         <Text>Ethereum Wallet demo</Text>
-        <Text>{this.props.web3.web3Instance._provider.timeout}</Text>
+        <Text>{this.state.isConnected?'Connected to rinkeby node':'Not Connected'}</Text>
+        <Text>{this.props.account}</Text>
       </View>
     )
   }
@@ -34,13 +53,19 @@ class Web3Info extends Component {
 
 const mapStateToProps = state => ({
         web3: state.web3,
+        account: state.reducers.account,
 })
+
+const mapDispatchToProps = (dispatch) => {
+  // Action
+  return {
+    STPupdateAccounts: (account0) => dispatch(STPupdateAccounts(account0)),
+  };
+};
 
 export default connect (
   mapStateToProps,
-  {
-    STPupdateAccounts,
-  }
+  mapDispatchToProps,
 ) (Web3Info)
 
 const styles = StyleSheet.create({
