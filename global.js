eeba46fd6c540f3af18b4c8bef76b7e581b47050
Victor Yeo <you@example.com>
2020-06-12 16:44:23 +0800

    add global crypto

--- global.js
+++ global.js
@@ -13,3 +13,17 @@ if (typeof atob === 'undefined') {
     return new Buffer(b64Encoded, 'base64').toString('binary');
   };
 }
+
+global.crypto = {
+  /*getRandomValues(byteArray) {
+    for (let i = 0; i < byteArray.length; i++) {
+      byteArray[i] = Math.floor(256 * Math.random());
+    }
+  },
+  getRandomBytes(size) {
+    for (let i = 0; i < size; i++) {
+      randombytes[i] = Math.floor(256 * Math.random());
+    }
+    return randombytes;
+  },*/
+};
