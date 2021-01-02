2630d1354182e4bc6d47b147ffa1606fda666063
Victor Yeo <victoryeo1974@gmail.com>
2021-01-02 20:35:11 +0800

    add Send component

---
+++ src/components/Send.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+import {View, Text} from 'react-native'
+
+export default function Send(props) {
+  return(
+    <View>
+      <Text>Send a Transaction</Text>
+    </View>
+  )
+}
